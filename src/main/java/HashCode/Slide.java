b5b82be1ffb7df8a61b469654dc6434404ce2c39
Ryxuma <tryfan99@gmail.com>
2019-02-28 19:31:03 +0000

    added tags to slide

--- src/main/java/HashCode/Slide.java
+++ src/main/java/HashCode/Slide.java
@@ -1,5 +1,8 @@
 package HashCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Slide {
 
     public final boolean isVerticalPair;
@@ -7,15 +10,22 @@ public class Slide {
     public Photo p1;
     public Photo p2;
 
+    public Set<String> tags = new HashSet<>();
+
     public Slide(Photo p) {
         this.p1 = p;
         isVerticalPair = false;
+
+        tags.addAll(p.getTags());
     }
 
     public Slide(Photo p1, Photo p2) {
         isVerticalPair = true;
         this.p1 = p1;
         this.p2 = p2;
+
+        tags.addAll(p1.getTags());
+        tags.addAll(p2.getTags());
     }
 
     @Override
