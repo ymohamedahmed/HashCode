2d654489da87a379dec99d793c2f605c8307cecb
Ryxuma <tryfan99@gmail.com>
2019-02-28 19:08:36 +0000

    Finished PairMaker

--- src/main/java/HashCode/PairMaker.java
+++ src/main/java/HashCode/PairMaker.java
@@ -8,21 +8,35 @@ import java.util.List;
 public class PairMaker {
 
     public HashSet<Slide> makePairs(List<Photo> photos) {
-        Collection<Photo> photosCopy = new ArrayList<>(photos);
+        List<Photo> photosCopy = new ArrayList<>(photos);
 
         // Greedy O(N^2)
 
         HashSet<Slide> slides = new HashSet<>();
 
         while(photosCopy.size() > 0) {
-            Photo p = photos.get(0);
-            photos.remove(0);
+            Photo p = photosCopy.get(0);
+            photosCopy.remove(0);
 
             if(p.isVertical()) {
 
                 Photo bestPair = null;
+                int leastSharedTags = 99999;
 
+                for(Photo p2 : photosCopy) {
+                    int st = Photo.tagsInCommon(p, p2);
+                    if(st < leastSharedTags) {
+                        bestPair = p2;
+                        leastSharedTags = st;
+                    }
 
+                    if(leastSharedTags == 0)
+                        break;
+                }
+
+                photosCopy.remove(bestPair);
+
+                slides.add(new Slide(p, bestPair));
             }
             else {
                 slides.add(new Slide(p));
