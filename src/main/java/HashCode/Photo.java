98c49f86db3c7fc30335ac904b62c9df2cb79fde
EmptyJackson <matthew@hmjackson.com>
2019-02-28 18:35:01 +0000

    Added Photo class

---
+++ src/main/java/HashCode/Photo.java
@@ -0,0 +1,24 @@
+package HashCode;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Photo {
+
+    private boolean isVertical;
+    private Set<String> tags;
+    private int id;
+
+    public Photo(char direction, int id, Collection<String> tags) {
+        this.tags = new HashSet<>(tags);
+        isVertical = (direction == 'V');
+        this.id = id;
+    }
+
+    public Set<String> getTags() { return tags; }
+
+    public int getId() { return id;}
+
+    public boolean isVertical() { return isVertical; }
+}
